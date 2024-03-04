1c82775dec215c481b30342af3c227fe4d21a355
Jessica Choi <jesymchoi@gmail.com>
2024-03-03 19:46:21 -0800

    answered questions

---
+++ app.js
@@ -0,0 +1,9 @@
+// - What is HTTP? - Hyper Text Transfer Protocol: how clients get and send data to a server
+// - What is a URL? - Uniform Resource Locator: an address for something on the internet
+// - What is DNS? - Domain Name System: takes URLs and converts them to IP addresses
+// - What is a query string? - Allows users to pass key-value pairs into URLs
+// - What are two HTTP verbs and how are they different? - Get: get data from a server; Post: send data to a server
+// - What is an HTTP request? - A request from a client to a server
+// - What is an HTTP response? - Response from a server to a client
+// - What is an HTTP header? Give a couple examples of request and response headers you have seen. - Something that provides additional information about the request or response (ie. Host, Accept, Last-Modified, etc.)
+// - What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser? - DNS takes URL and converts to an IP address, server sends a response, browswer takes response, sees what else is needed, and sends other requests for those things
